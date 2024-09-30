893fbeaa757079afab99af61b62e91a79680386a
adamtomi <atomi2500@gmail.com>
2024-09-30 13:22:06 +0300

    add serialVersionUID

--- src/main/java/com/tomushimano/waypoint/util/DontInvokeMe.java
+++ src/main/java/com/tomushimano/waypoint/util/DontInvokeMe.java
@@ -1,6 +1,10 @@
 package com.tomushimano.waypoint.util;
 
+import java.io.Serial;
+
 public class DontInvokeMe extends UnsupportedOperationException {
+    @Serial
+    private static final long serialVersionUID = -8745612180694336052L;
 
     public DontInvokeMe() {
         super("This constructor is private as it's not meant to be invoked.");
