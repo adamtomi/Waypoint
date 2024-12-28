719560c590d06af73d356cc8b5b2d4f48798aeff
adamtomi <atomi2500@gmail.com>
2024-12-28 16:20:14 +0100

    update grapefruit

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,9 @@ dependencies {
     annotationProcessor("com.google.dagger:dagger-compiler:2.52")
 
     // Libs
-    implementation(files("libs/grapefruit-3.0.0-ALPHA.jar"))
+    implementation(fileTree("./libs") {
+        include("*.jar")
+    })
     implementation("com.zaxxer:HikariCP:5.1.0") {
         exclude("org.slf4j")
     }
