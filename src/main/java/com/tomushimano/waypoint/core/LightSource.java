1d54eda9733e4aae013fbc514eab8648a03f3ac0
adamtomi <atomi2500@gmail.com>
2026-05-20 14:44:02 +0200

    add WaypointElement interface

--- src/main/java/com/tomushimano/waypoint/core/LightSource.java
+++ src/main/java/com/tomushimano/waypoint/core/LightSource.java
@@ -1,13 +1,8 @@
 package com.tomushimano.waypoint.core;
 
 import com.tomushimano.waypoint.util.Position;
-import org.bukkit.entity.Player;
 
-public interface LightSource {
-
-    void show(final Player player);
-
-    void hide(final Player player);
+public interface LightSource extends WaypointElement {
 
     static LightSourceImpl at(final Position position) {
         return new LightSourceImpl(position);
---
+++ src/main/java/com/tomushimano/waypoint/core/WaypointElement.java
@@ -0,0 +1,10 @@
+package com.tomushimano.waypoint.core;
+
+import org.bukkit.entity.Player;
+
+public interface WaypointElement {
+    
+    void show(final Player player);
+
+    void hide(final Player player);
+}
